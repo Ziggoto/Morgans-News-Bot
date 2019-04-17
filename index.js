063d4628b36dc9d3808562542b1ca3e757a4d0db
Fábio Theophilo <fabiotheophilo@gmail.com>
2019-04-17 16:42:11 +0100

    Refactores

--- index.js
+++ index.js
@@ -4,46 +4,82 @@ const config = require('dotenv').config()
 
 const { JSDOM } = jsdom
 
+const selectors = {
+  postContent: 'div[data-test-id="post-content"]'
+}
+
 const formatUrl = (route) => `https://www.reddit.com${route}`
 
 const fetchPost = () => {
-    return fetch('https://www.reddit.com/r/OnePiece/')
-        .then(response => response.text())
+  console.log('Fetching /r/OnePiece')
+  return fetch('https://www.reddit.com/r/OnePiece/')
+    .then(response => response.text())
+}
+
+const fetchMangaUrl = (postUrl) => {
+  console.log(`Fetching ${postUrl}`)
+  return fetch(postUrl)
+    .then(response => response.text())
 }
 
 const sendMessageOnTelegram = (url) => {
-    const { ACCESS_TOKEN } = process.env
+  const { ACCESS_TOKEN } = process.env
+  console.log('Sending message to Telegram')
 
-    fetch(`https://api.telegram.org/bot${ACCESS_TOKEN}/sendMessage`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'chat_id': 59761682,
-            text: `Opa, boas noticias: ${url}`
-        })
+  fetch(`https://api.telegram.org/bot${ACCESS_TOKEN}/sendMessage`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'chat_id': 59761682,
+      text: `Mangá anunciado! ${url}`
     })
+  })
 }
 
-const main = async () => {
-    const htmlDocument = await fetchPost()
+const parsePostList = (htmlDocument) => {
+  return new Promise((resolve, reject) => {
     const { document } = new JSDOM(htmlDocument).window
 
     const headers = [...document.querySelectorAll('span')]
-        .filter(el => el.textContent.match(/Current Chapter/))
+      .filter(el => el.textContent.match(/Current Chapter/))
+      .map(el => el.parentElement.href)
+      .map(formatUrl)
 
-    const links = headers
-        .map(el => el.parentElement.href)
-        .map(formatUrl)
-
-    if (links.length > 0) {
-        console.log(`Encontrado: ${links[0]}`)
-        sendMessageOnTelegram(links[0])
-    } else {
-        console.log('Nenhum manga novo encontrado!')
+    if (headers.length > 0) {
+      console.log('Found: ', headers[0])
+      resolve(headers[0])
     }
+    console.error('Not found any manga, aborting...')
+    reject()
+  })
+}
+
+const parseMangaPost = (htmlDocument) => {
+  return new Promise((resolve, reject) => {
+    const { document } = new JSDOM(htmlDocument).window
+    const urls = [...document.querySelector(selectors.postContent).querySelectorAll('a')]
+      .map(a => a.href)
+      .filter(el => !el.match(/redit/g))
+
+    if (urls.length > 0) {
+      console.log('Found: ', urls[0])
+      resolve(urls[0])
+    }
+    console.error('Not found any url link, aborting...')
+    reject()
+  })
+}
+
+const main = () => {
+  fetchPost()
+    .then(parsePostList)
+    .then(fetchMangaUrl)
+    .then(parseMangaPost)
+    .then(sendMessageOnTelegram)
+    .catch(err => console.err)
 }
 
 main()
